86bad2751c16c9453a052f42cc86520a9064b046
Simon Edland <Simon.vestvik2000@gmail.com>
2022-04-07 14:23:32 +0200

    prepping to merge

--- microsegmenter.py
+++ microsegmenter.py
@@ -1,3 +1,7 @@
+#how to solve
+#i think i need to alocate one nettwork to eatch spine and then make then microsegment the networks to the leafs.
+#the leafs then knowing what nic goes to what spine can configgure to use the right ip based on it knowing what spine it self is
+#would like to know a beter way to do this
 
 from nornir_utils.plugins.functions import print_result
 from nornir_netmiko.tasks import netmiko_send_command, netmiko_send_config
