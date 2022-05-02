42f5b32568c34fd47975c2b7551d1cbb2a6373a6
Sion VEstvik Edland <Simon.Vestvik2000@gmail.com>
2022-05-02 09:51:39 +0200

    prepp the l1/l2 sec module

--- portSecurer.py
+++ portSecurer.py
@@ -1,2 +1,10 @@
-def PortSecurer():
-    print("PortSecurer")
+def PortMarker():
+    print("test")
+def PortCloser():
+    print("test")
+def DHCPMarker():
+    print("test")
+def ARPMarker():
+    print("test")
+def MACLimiter():
+    print("test")
